a161154e828af1adb2958965d1a315918fa721c2
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-31 06:37:00 +0700

    add Proxy record

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,13 @@ import Text.HTML.Scalpel
 import Control.Applicative
 import Data.List.Split
 import qualified Data.Text as T
+
+data Proxy =
+  Proxy {ip::String,
+         port::String,
+         code::String,
+         country::String,
+         anonymity::String} deriving (Show)
   
 proxiesRawHtml :: IO (Maybe [String])
 proxiesRawHtml = scrapeURL "https://free-proxy-list.net/" (htmls "tr")
@@ -12,23 +19,25 @@ proxiesRawHtml = scrapeURL "https://free-proxy-list.net/" (htmls "tr")
 replaceWithBlank :: String -> [String] -> String
 replaceWithBlank x xs = foldl (\acc x -> T.unpack (T.replace (T.pack x) (T.pack "")  (T.pack acc))) x xs
 
-mapToProxies :: [String] -> [[String]]
+mapToProxies :: [String] -> [Proxy]
 mapToProxies xs =
   init $ tail
        $ map
-         (\x -> splitOn "</td>" x)
-         xs  
+           (\x -> Proxy{ip=replaceWithBlank (x !! 0) ["<tr>", "<td>"],
+                        port=replaceWithBlank (x !! 1) ["<td>"],
+                        code=replaceWithBlank (x !! 2) ["<td>"],
+                        country=replaceWithBlank (x !! 3) ["<td class=\"hm\">"],
+                        anonymity=replaceWithBlank (x !! 4) ["<td>"]})
+           (map
+             (\x -> splitOn "</td>" x)
+             xs) 
+           
           
 main :: IO ()
 main = do
   xxs <- proxiesRawHtml
   case xxs of
-    Just xs -> putStrLn $ show (mapToProxies xs)
+    Just xs -> putStrLn $ show (head $ mapToProxies xs)
       
   return ()
-
-
-
-
-
 
